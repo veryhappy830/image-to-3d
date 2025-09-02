8649af36186c0f102fcda54639783fcf00ce5d87
mini <mini@MacMini-RDR-1.local>
2025-09-02 15:56:09 +0700

    fix: fix simple i23

---
+++ setup_env.sh
@@ -0,0 +1,3 @@
+pip install torch==2.4.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+pip install kaolin==0.18.0 -f https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-2.4.0_cu121.html
+pip install diffusers accelerate sentencepiece fastapi uvicorn omegaconf loguru python-multipart
