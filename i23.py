d88916802f7576a04b1828e0deaf34e5e8bbbacc
mini <mini@MacMini-RDR-1.local>
2025-09-02 15:50:06 +0700

    feat: add simple i23

---
+++ i23.py
@@ -0,0 +1,36 @@
+import os
+# os.environ['ATTN_BACKEND'] = 'xformers'   # Can be 'flash-attn' or 'xformers', default is 'flash-attn'
+os.environ['SPCONV_ALGO'] = 'native'        # Can be 'native' or 'auto', default is 'auto'.
+                                            # 'auto' is faster but will do benchmarking at the beginning.
+                                            # Recommended to set to 'native' if run only once.
+
+import imageio
+import torch
+from PIL import Image
+from trellis.pipelines import TrellisImageTo3DPipeline
+from trellis.utils import render_utils, postprocessing_utils
+
+# Load a pipeline from a model folder or a Hugging Face model hub.
+pipeline = TrellisImageTo3DPipeline.from_pretrained("microsoft/TRELLIS-image-large")
+pipeline.cuda()
+
+# Load an image
+image = Image.open("./output.png")
+
+# Run the pipeline
+outputs = pipeline.run(
+    image,
+    seed=1
+)
+
+# outputs is a dictionary containing generated 3D assets in different formats:
+# - outputs['gaussian']: a list of 3D Gaussians
+# - outputs['radiance_field']: a list of radiance fields
+# - outputs['mesh']: a list of meshes
+
+# Render the outputs
+video = render_utils.render_video(outputs['gaussian'][0])['color']
+imageio.mimsave("output.mp4", video, fps=30)
+
+# Save Gaussians as PLY files
+outputs['gaussian'][0].save_ply("output.ply")
