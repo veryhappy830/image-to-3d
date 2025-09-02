e444b73e5d32a0b069f87e14ebc86d4f5c3a84d9
mini <mini@MacMini-RDR-1.local>
2025-09-02 16:08:45 +0700

    fix: fix setup env

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,3 @@
 pip install torch==2.4.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip install kaolin==0.18.0 -f https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-2.4.0_cu121.html
-pip install diffusers accelerate sentencepiece fastapi uvicorn omegaconf loguru python-multipart
+pip install nvdiffrast imageio easydict rembg onnxruntime transformers open3d plyfile utils3d diffusers accelerate sentencepiece fastapi uvicorn omegaconf loguru python-multipart
